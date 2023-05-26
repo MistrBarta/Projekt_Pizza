1dc2fe7016f269601308965a29dce84e74f99648
Mistrbarta <AdamBarta12@seznam.cz>
2023-05-26 21:08:32 +0200

    sdf

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,43 +1,57 @@
 import React from 'react'
 import "../Styles/Pizza.css"
-import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom";
 
 const Pizza = (props) => {
-    const navigate = useNavigate();
-    const deletePizza = async () => {
-      const res = await fetch(`http://localhost:3000/pizza/${props.id}`, {
-      method: "DELETE",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    navigate(0)
-  }
+    const { id } = useParams()
+    const [pizza, setPizza] = useState({});
+    const [loaded, setLoaded] = useState(false);
 
-  const handleDelete = () => {
-    deletePizza()
-  }
+    const getPizza = async () => {
+        const res = await fetch(`http://localhost:3000/pizza/${id}`, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            method: "GET",
+        });
+        const data = await res.json();
+        setPizza(data);
+        setLoaded(true);
+    }
+
+    useEffect(() => {
+        getPizza();
+        console.log(pizza.result)
+    }, [])
+
+    if (!loaded)
+        return (
+            <>
+                <p>Loading pizza...</p>
+            </>
+        )
 
   return (
-    <div className='tempalate-page'>
-        <Link to={`pizza/${props.id}`} className="column">
-            <div className='tempalate-content'>
-                <div className='template-tittle'>
-                    <p>{props.name}</p>
-                </div>
-                <div className='template-ing'>
-                    <p>{props.ingredients}</p>
-                </div>
-                <div className='template-sale'>
-                    <div className='template-price'>
-                        <p>{props.price}</p>
-                    </div>
-                    <button className='template-button'>Koupit</button>
-                </div>
-            </div>
-        </Link>
-    </div>
-
+    <Link to={`pizza/${props.id}`} className="column">
+      <div className='tempalate-page'>
+              <div className='tempalate-content'>
+                  <div className='template-tittle'>
+                      <p>{pizza.result[0].name}</p>
+                  </div>
+                  <div className='template-ing'>
+                      <p>{pizza.result[0].ingredients}</p>
+                  </div>
+                  <div className='template-sale'>
+                      <div className='template-price'>
+                          <p>{pizza.result[0].price}</p>
+                      </div>
+                      <button className='template-button'>Koupit</button>
+                  </div>
+              </div>
+      </div>
+    </Link>
     /*Přesunout template.js/css a Pizza.js*/
   )
 }
